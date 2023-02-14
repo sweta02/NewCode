082f4aab760f07ee3d9f2df6135658838778f7fc
sweta patel <sweta_0279@yahoo.co.uk>
2023-02-14 00:56:31 +0000

    new code

---
+++ src/main/java/NewCode/UniCo.java
@@ -0,0 +1,15 @@
+package NewCode;
+
+public class UniCo {
+
+    /* today we learned how to push new code or projects
+    in github
+    This is practise lesson
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Automation is good");
+        System.out.println("lets push this code in github");
+
+    }
+}
