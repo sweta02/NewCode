62d3b402816896cf9d5519df05291fb32846cfae
sweta patel <sweta_0279@yahoo.co.uk>
2023-02-14 01:13:30 +0000

    name

---
+++ src/main/java/NewCode/NameClass.java
@@ -0,0 +1,9 @@
+package NewCode;
+
+public class NameClass {
+    public static void main(String[] args) {
+        System.out.println("Hi I am going to become a Tester");
+        System.out.println("it is going to be interesting class");
+
+    }
+}
